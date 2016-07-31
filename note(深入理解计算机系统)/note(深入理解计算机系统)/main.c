f3715c8d138494bff25e5ad0a328aea1fc58b9ef
Your Name <you@example.com>
2016-07-31 10:27:26 +0800

    配置

---
+++ note(深入理解计算机系统)/note(深入理解计算机系统)/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  note(深入理解计算机系统)
+//
+//  Created by 郭利辉 on 16/7/31.
+//  Copyright © 2016年 郭利辉. All rights reserved.
+//
+
+#include <stdio.h>
+//配置
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
